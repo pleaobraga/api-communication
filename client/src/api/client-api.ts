f10227bc062c41db948c2df5bc9035f83b3a18ec
pleaobraga <pleao.braga@gmail.com>
2025-04-05 17:15:45 -0300

    feat: add graphql client

--- client/src/api/client-api.ts
+++ client/src/api/client-api.ts
@@ -2,27 +2,26 @@ import { Comment } from '@/@types'
 import { graphQLAPI } from './graphql'
 import { restAPI } from './rest'
 
-const COMMUNICATIONS = {
-  rest: { apis: restAPI, type: 'rest' },
-  graphQL: { apis: graphQLAPI, type: 'graphQL' }
-}
-
-type Props = {
+type APIImplementation = {
   updateCommentAPI: (
     id: string,
     dto: { content: string }
   ) => Promise<{ comment: Comment }>
 }
 
-function apiImplementation(APIFunctions: Props) {
-  return {
-    ...APIFunctions
-  }
+type CommunicationType = 'rest' | 'graphQL'
+
+interface Communication {
+  apis: APIImplementation
+  type: CommunicationType
+}
+
+const COMMUNICATIONS: Record<CommunicationType, Communication> = {
+  rest: { apis: restAPI, type: 'rest' },
+  graphQL: { apis: graphQLAPI, type: 'graphQL' }
 }
 
 const apiType = process.env.API_COMMUNICATION as keyof typeof COMMUNICATIONS
 
-const implementationAPI =
+export const clientAPI =
   COMMUNICATIONS[apiType]?.apis ?? COMMUNICATIONS.rest.apis
-
-export const clientAPI = apiImplementation(implementationAPI)
